14db2aa5dab21e34d723f69dd6192584ae253834
noeyigg <miyg2003@naver.com>
2025-11-04 17:34:05 +0900

    test: 한글 인코딩 테스트

--- src/main/java/gighub/worketserver/WorketServerApplication.java
+++ src/main/java/gighub/worketserver/WorketServerApplication.java
@@ -8,5 +8,6 @@ public class WorketServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(WorketServerApplication.class,args);
+    System.out.println("한글 인코딩 테스트");
   }
 }
