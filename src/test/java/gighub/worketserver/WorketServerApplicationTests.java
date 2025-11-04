43db7098f1354bd26bb0198a7614e47600c6be54
ruudska6 <feels19@naver.com>
2025-11-04 10:20:42 +0900

    setting: 스프링부트 세팅

---
+++ src/test/java/gighub/worketserver/WorketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package gighub.worketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
