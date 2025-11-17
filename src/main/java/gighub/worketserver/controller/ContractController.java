62db3ab3bb05c68c1ddb2c581b969f1ff4739851
youm0524 <juliet0524@naver.com>
2025-11-17 16:09:22 +0900

    feat: 계약서 컨트롤러 기능

---
+++ src/main/java/gighub/worketserver/controller/ContractController.java
@@ -0,0 +1,27 @@
+package gighub.worketserver.controller;
+
+import gighub.worketserver.global.response.ApiResponse;
+import gighub.worketserver.service.ContractUploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequestMapping("/contracts")
+@RestController
+@RequiredArgsConstructor
+public class ContractController {
+
+  private final ContractUploadService contractService;
+
+  @PostMapping("/extract")
+  public ApiResponse<?> extract(
+    @RequestPart("file") MultipartFile file,
+    @RequestPart("message") String message
+  ) {
+    return ApiResponse.ok(contractService.process(file, message));
+  }
+}
