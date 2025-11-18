1e8c4c16f771a3da211001ab671701e337b82613
ruudska6 <feels19@naver.com>
2025-11-18 09:38:32 +0900

    fix: rotate를 만들어놓고 빼먹어서 다시 넣음

--- src/main/java/gighub/worketserver/global/filter/TokenAuthenticationFilter.java
+++ src/main/java/gighub/worketserver/global/filter/TokenAuthenticationFilter.java
@@ -54,14 +54,17 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
             // refresh token 만료 임박 여부 확인 후 재발급 (잔여기간 1일 이하)
             if (refreshTokenService.isExpiringSoon(refreshToken, 24)) {
+
               String newRefreshToken = tokenProvider.reissueRefreshToken(refreshToken);
+
+              refreshTokenService.rotateRefreshToken(refreshToken, newRefreshToken);
 
               ResponseCookie refreshCookie = cookieUtil.createTokenCookie("refreshToken", newRefreshToken, 7 * 24 * 60 * 60);
               response.addHeader("Set-Cookie", refreshCookie.toString());
             }
           }
 
-        }  else {
+        } else {
           // 3. 두 토큰 모두 기간이 유효하지 않음 => 쿠키를 비워줌 => 다시 로그인 해야함
           throw new TokenException(TokenErrorCode.EXPIRED_TOKEN);  // 401
         }
