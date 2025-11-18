2e62cd18cb831c1eaa341893abbcf6eeeeae0a8f
ruudska6 <feels19@naver.com>
2025-11-18 15:16:50 +0900

    fix: 일관된 응답을 위해 ApiResponse 타입으로 변경

--- src/main/java/gighub/worketserver/global/exception/GlobalExceptionHandler.java
+++ src/main/java/gighub/worketserver/global/exception/GlobalExceptionHandler.java
@@ -1,28 +1,26 @@
 package gighub.worketserver.global.exception;
 
-import org.springframework.http.ResponseEntity;
+import gighub.worketserver.global.response.ApiResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
-  /*
-   * Developer Custom Exception: 직접 정의한 RestApiException 에러 클래스에 대한 예외 처리
+  /**
+   * ErrorCode 기반 Custom Exception 처리
    */
   @ExceptionHandler(RestApiException.class)
-  protected ResponseEntity<ErrorResponse> handleCustomException(RestApiException ex) {
+  protected ApiResponse<?> handleRestApiException(RestApiException ex) {
     ErrorCode errorCode = ex.getErrorCode();
-    return handleExceptionInternal(errorCode);
+    return ApiResponse.error(errorCode.getMessage());
   }
 
-  ;
-
-  // handleExceptionInternal() 메소드를 오버라이딩해 응답 커스터마이징
-  private ResponseEntity<ErrorResponse> handleExceptionInternal(ErrorCode errorCode) {
-    return ResponseEntity
-      .status(errorCode.getHttpStatus().value())
-      .body(new ErrorResponse(errorCode));
+  /**
+   * 예상 못한 모든 예외 처리
+   */
+  @ExceptionHandler(Exception.class)
+  protected ApiResponse<?> handleException(Exception ex) {
+    return ApiResponse.error(ex.getMessage());
   }
 }
