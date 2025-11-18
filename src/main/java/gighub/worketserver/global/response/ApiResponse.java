2ecd0c34bf08c50e004e340fbd3d1344fe9dd595
ruudska6 <feels19@naver.com>
2025-11-18 15:36:31 +0900

    style: code 위치 변경으로 좀 더 잘볼수있게 함

--- src/main/java/gighub/worketserver/global/response/ApiResponse.java
+++ src/main/java/gighub/worketserver/global/response/ApiResponse.java
@@ -8,17 +8,18 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class ApiResponse<T> {
-  
+
   private static final String SUCCESS = "success";
   private static final String ERROR = "error";
 
   private String status;
   @JsonInclude(JsonInclude.Include.NON_NULL)
+  private String code;
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   private T data;
   @JsonInclude(JsonInclude.Include.NON_NULL)
   private String errorMessage;
-  @JsonInclude(JsonInclude.Include.NON_NULL)
-  private String code;
+
 
   public static <T> ApiResponse<T> ok(T data) {
     return ApiResponse.<T>builder()
