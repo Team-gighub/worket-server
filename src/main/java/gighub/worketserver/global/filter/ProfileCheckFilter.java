6c6fd2c1b71caad42922a0e6f1e07894aa83eb13
ruudska6 <feels19@naver.com>
2025-11-19 11:41:11 +0900

    refactor: Profile 체크할 때 Freelancer에서 끌어오도록 변경

--- src/main/java/gighub/worketserver/global/filter/ProfileCheckFilter.java
+++ src/main/java/gighub/worketserver/global/filter/ProfileCheckFilter.java
@@ -1,10 +1,12 @@
 package gighub.worketserver.global.filter;
 
+import gighub.worketserver.domain.FreelancerProfile;
 import gighub.worketserver.domain.User;
 import gighub.worketserver.domain.constants.Role;
 import gighub.worketserver.global.exception.ProfileErrorCode;
 import gighub.worketserver.global.exception.ProfileException;
 import gighub.worketserver.global.security.dto.PrincipalDetails;
+import gighub.worketserver.repository.FreelancerProfileRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,10 +18,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class ProfileCheckFilter extends OncePerRequestFilter {
+  private final FreelancerProfileRepository freelancerProfileRepository;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
@@ -35,7 +39,10 @@ public class ProfileCheckFilter extends OncePerRequestFilter {
 
       // 프리랜서 프로필 체크
       if (user.getRole() == Role.FREELANCER) {
-        if (user.getFreelancerProfile() == null) {
+        Long userId = user.getId();
+        Optional<FreelancerProfile> profileOpt = freelancerProfileRepository.findByUserId(userId);
+
+        if (profileOpt.isEmpty()) {
           request.setAttribute("exception", ProfileErrorCode.FREELANCER_PROFILE_NOT_FOUND);
           throw new ProfileException(ProfileErrorCode.FREELANCER_PROFILE_NOT_FOUND);
         }
