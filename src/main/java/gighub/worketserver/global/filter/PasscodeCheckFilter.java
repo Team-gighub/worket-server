808dd86b6d96f274ae5caf7c33517f3b4e1ab54f
ruudska6 <feels19@naver.com>
2025-11-20 17:08:12 +0900

    refactor: 필터에서 쓰는 예외 이름 변경

--- src/main/java/gighub/worketserver/global/filter/PasscodeCheckFilter.java
+++ src/main/java/gighub/worketserver/global/filter/PasscodeCheckFilter.java
@@ -1,8 +1,8 @@
 package gighub.worketserver.global.filter;
 
 import gighub.worketserver.domain.User;
-import gighub.worketserver.global.exception.PasscodeErrorCode;
-import gighub.worketserver.global.exception.PasscodeException;
+import gighub.worketserver.global.exception.PasscodeForFilterErrorCode;
+import gighub.worketserver.global.exception.PasscodeForFilterException;
 import gighub.worketserver.global.security.dto.PrincipalDetails;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -20,30 +20,30 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class PasscodeCheckFilter extends OncePerRequestFilter {
 
-  @Override
-  protected boolean shouldNotFilter(HttpServletRequest request) {
-    String uri = request.getRequestURI();
-    return uri.startsWith("/test")
-      || uri.startsWith("/oauth2")
-      || uri.startsWith("/auth/passcode");
-  }
-
-  @Override
-  protected void doFilterInternal(HttpServletRequest request,
-                                  HttpServletResponse response,
-                                  FilterChain filterChain)
-    throws ServletException, IOException {
-
-    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
-    PrincipalDetails details = (PrincipalDetails) auth.getPrincipal();
-    User user = details.getUser();
-
-    if (user.getPasscode() == null || user.getPasscode().isBlank()) {
-      request.setAttribute("exception", PasscodeErrorCode.PASSCODE_EMPTY);
-      throw new PasscodeException(PasscodeErrorCode.PASSCODE_EMPTY);
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        return uri.startsWith("/test")
+                || uri.startsWith("/oauth2")
+                || uri.startsWith("/auth/passcode");
     }
 
-    filterChain.doFilter(request, response);
-  }
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        PrincipalDetails details = (PrincipalDetails) auth.getPrincipal();
+        User user = details.getUser();
+
+        if (user.getPasscode() == null || user.getPasscode().isBlank()) {
+            request.setAttribute("exception", PasscodeForFilterErrorCode.PASSCODE_EMPTY);
+            throw new PasscodeForFilterException(PasscodeForFilterErrorCode.PASSCODE_EMPTY);
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
