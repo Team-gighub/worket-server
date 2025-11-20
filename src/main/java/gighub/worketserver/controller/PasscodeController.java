145c170d524e54814957c80c2c035d83aed1a1be
ruudska6 <feels19@naver.com>
2025-11-20 15:43:25 +0900

    feat: 패스코드 등록과 검증을 위한 PasscodeController 구현

---
+++ src/main/java/gighub/worketserver/controller/PasscodeController.java
@@ -0,0 +1,47 @@
+package gighub.worketserver.controller;
+
+import gighub.worketserver.global.response.ApiResponse;
+import gighub.worketserver.service.PasscodeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth/passcode")
+public class PasscodeController {
+
+  private final PasscodeService passcodeService;
+
+  @PostMapping("/register")
+  public ApiResponse<?> register(
+    @RequestBody Map<String, String> body,
+    Authentication authentication
+  ) {
+    Long userId = Long.parseLong(authentication.getName());
+    String passcode = body.get("passcode");
+    System.out.println("패스코드 왓어용" + passcode);
+
+    passcodeService.registerPasscode(userId, passcode);
+
+    return ApiResponse.ok("패스코드 등록 완료");
+  }
+
+  @PostMapping("/verify")
+  public ApiResponse<?> verify(
+    @RequestBody Map<String, String> body,
+    Authentication authentication
+  ) {
+    Long userId = Long.parseLong(authentication.getName());
+    String passcode = body.get("passcode");
+
+    passcodeService.verifyPasscode(userId, passcode);
+
+    return ApiResponse.ok("패스코드 인증 성공");
+  }
+}
