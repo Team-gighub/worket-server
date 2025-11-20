009dd7a6938f927c00babda09d4bd62214edbad4
SooY2 <ssy67366736@gmail.com>
2025-11-20 14:18:17 +0900

    chore: 안쓰는 import제거

--- src/main/java/gighub/worketserver/repository/TransactionRepository.java
+++ src/main/java/gighub/worketserver/repository/TransactionRepository.java
@@ -2,7 +2,6 @@ package gighub.worketserver.repository;
 
 import gighub.worketserver.domain.Transaction;
 import gighub.worketserver.domain.constants.TransactionStatus;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
