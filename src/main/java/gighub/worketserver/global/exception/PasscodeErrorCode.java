3bf294af157083e8f8a7299f42b3c44fb3157d3b
ruudska6 <feels19@naver.com>
2025-11-20 17:07:47 +0900

    feat: passcode 예외 구현

--- src/main/java/gighub/worketserver/global/exception/PasscodeErrorCode.java
+++ src/main/java/gighub/worketserver/global/exception/PasscodeErrorCode.java
@@ -7,11 +7,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum PasscodeErrorCode implements ErrorCode {
+
     PASSCODE_EMPTY(HttpStatus.FORBIDDEN, "AUTH_2001", "패스코드가 등록되지 않았습니다."),
     INVALID_PASSCODE(HttpStatus.UNAUTHORIZED, "AUTH_2002", "잘못된 패스코드입니다.");
 
     private final HttpStatus httpStatus;
     private final String customCode;
     private final String message;
-}
 
+}
