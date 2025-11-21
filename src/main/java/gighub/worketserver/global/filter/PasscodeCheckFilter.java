ce83484b5cc7e5375632213a5b8d252fd5b6e6bc
ruudska6 <feels19@naver.com>
2025-11-21 15:28:25 +0900

    delete: auth 중복 제거

--- src/main/java/gighub/worketserver/global/filter/PasscodeCheckFilter.java
+++ src/main/java/gighub/worketserver/global/filter/PasscodeCheckFilter.java
@@ -25,7 +25,6 @@ public class PasscodeCheckFilter extends OncePerRequestFilter {
     String uri = request.getRequestURI();
     return uri.startsWith("/test")
       || uri.startsWith("/oauth2")
-      || uri.startsWith("/auth")
       || uri.startsWith("/auth/passcode")
       || uri.matches("^/transactions/\\d+/preview$");
   }
