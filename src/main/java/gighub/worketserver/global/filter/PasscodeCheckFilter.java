62bdd9e4880a7da48f438f2028e9ed7b3d652352
ruudska6 <feels19@naver.com>
2025-11-24 11:30:41 +0900

    merge

--- src/main/java/gighub/worketserver/global/filter/PasscodeCheckFilter.java
+++ src/main/java/gighub/worketserver/global/filter/PasscodeCheckFilter.java
@@ -25,7 +25,7 @@ public class PasscodeCheckFilter extends OncePerRequestFilter {
     String uri = request.getRequestURI();
     return uri.startsWith("/test")
       || uri.startsWith("/oauth2")
-      || uri.startsWith("/auth")
+      || uri.startsWith("/auth/passcode")
       || uri.startsWith("/users/me")
       || uri.matches("^/transactions/\\d+/preview$");
   }
