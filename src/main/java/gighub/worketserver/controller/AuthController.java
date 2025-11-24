d4caa974684908274de7af96f2e94936997b2e62
ruudska6 <feels19@naver.com>
2025-11-24 15:21:25 +0900

    refactor: PasscodeController -> AuthController 엔드포인트 안에 auth가 붙으므로 안으로 변경

--- src/main/java/gighub/worketserver/controller/AuthController.java
+++ src/main/java/gighub/worketserver/controller/AuthController.java
@@ -10,12 +10,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth/passcode")
-public class PasscodeController {
+public class AuthController {
 
     private final PasscodeService passcodeService;
 
