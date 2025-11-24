3bb7c26c829b3ee09bc34eb289bb6d24123e0f74
SooY2 <ssy67366736@gmail.com>
2025-11-24 11:20:10 +0900

    fix: pdf에 역할을 한국어로 표시하기 위해 enum 수정

--- src/main/java/gighub/worketserver/domain/constants/Role.java
+++ src/main/java/gighub/worketserver/domain/constants/Role.java
@@ -1,7 +1,17 @@
 package gighub.worketserver.domain.constants;
 
 public enum Role {
-  FREELANCER, // 프리랜서
-  CLIENT, // 의뢰인
-  ADMIN // 관리자
+  FREELANCER("프리랜서"),
+  CLIENT("의뢰인"),
+  ADMIN("관리자");
+
+  private final String korName;
+
+  Role(String korName) {
+    this.korName = korName;
+  }
+
+  public String getKorName() {
+    return korName;
+  }
 }
