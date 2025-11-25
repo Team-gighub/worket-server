16271fbb99e493adf699419ff4cba07b51f1a586
SooY2 <ssy67366736@gmail.com>
2025-11-25 11:32:00 +0900

    feat: userRole에 따른 커스텀 에러 반환

--- src/main/java/gighub/worketserver/global/filter/PasscodeCheckFilter.java
+++ src/main/java/gighub/worketserver/global/filter/PasscodeCheckFilter.java
@@ -1,6 +1,7 @@
 package gighub.worketserver.global.filter;
 
 import gighub.worketserver.domain.User;
+import gighub.worketserver.domain.constants.Role;
 import gighub.worketserver.global.exception.PasscodeErrorCode;
 import gighub.worketserver.global.exception.PasscodeException;
 import gighub.worketserver.global.exception.PasscodeForFilterErrorCode;
@@ -44,7 +45,21 @@ public class PasscodeCheckFilter extends OncePerRequestFilter {
     PrincipalDetails details = (PrincipalDetails) auth.getPrincipal();
     User user = details.getUser();
 
+    Role role = user.getRole();
+
     if (user.getPasscode() == null || user.getPasscode().isBlank()) {
+
+      // 역할별 예외 코드 분기
+      if (role == Role.FREELANCER) {
+        request.setAttribute("exception", PasscodeForFilterErrorCode.PASSCODE_EMPTY_FREELANCER);
+        throw new PasscodeForFilterException(PasscodeForFilterErrorCode.PASSCODE_EMPTY_FREELANCER);
+      }
+
+      if (role == Role.CLIENT) {
+        request.setAttribute("exception", PasscodeForFilterErrorCode.PASSCODE_EMPTY_CLIENT);
+        throw new PasscodeForFilterException(PasscodeForFilterErrorCode.PASSCODE_EMPTY_CLIENT);
+      }
+
       request.setAttribute("exception", PasscodeForFilterErrorCode.PASSCODE_EMPTY);
       throw new PasscodeForFilterException(PasscodeForFilterErrorCode.PASSCODE_EMPTY);
     }
