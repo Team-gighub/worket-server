e1a32504beb3aefebc1a7f2f8ee410c62a1b67a4
SooY2 <ssy67366736@gmail.com>
2025-11-27 17:09:35 +0900

    feat: contractId로 contractFile 찾기

--- src/main/java/gighub/worketserver/repository/ContractFileRepository.java
+++ src/main/java/gighub/worketserver/repository/ContractFileRepository.java
@@ -4,12 +4,10 @@ import gighub.worketserver.domain.ContractFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ContractFileRepository extends JpaRepository<ContractFile, Long> {
-  @Override
-  Optional<ContractFile> findById(Long contractId);
+
+  ContractFile findByContractId(Long contractId);
 
 
 }
