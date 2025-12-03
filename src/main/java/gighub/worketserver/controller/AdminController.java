5456b52409b6d829086837c91c0b59cb92002910
youm0524 <juliet0524@naver.com>
2025-12-03 15:25:42 +0900

    feat: 관리자 기능에 사용자 통계조회 기능 추가

--- src/main/java/gighub/worketserver/controller/AdminController.java
+++ src/main/java/gighub/worketserver/controller/AdminController.java
@@ -1,5 +1,6 @@
 package gighub.worketserver.controller;
 
+import gighub.worketserver.dto.AdminStatsResponse;
 import gighub.worketserver.dto.ContractModificationDetail;
 import gighub.worketserver.dto.ContractModificationResponse;
 import gighub.worketserver.dto.ContractModifyRequest;
@@ -12,29 +13,38 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 /**
- * 계약서 수정 관련 API Controller(관리자 페이지)
+ * 관리자 페이지
+ * 1. 계약서 수정 요청
+ * 2. 사용자 통계 조회
  */
-@RequestMapping("/contract-modifications")
 @RestController
 @RequiredArgsConstructor
 public class AdminController {
   private final AdminService adminService;
 
-  @GetMapping
+  @GetMapping("/contract-modifications")
   public ApiResponse<List<ContractModificationResponse>> getModificationList() {
     List<ContractModificationResponse> responseList = adminService.getModificationList();
     return ApiResponse.ok(responseList);
   }
 
-  @GetMapping("/{id}")
+  @GetMapping("/contract-modifications/{id}")
   public ApiResponse<ContractModificationDetail> getModificationById(@PathVariable Long id) {
     ContractModificationDetail response = adminService.getModificationById(id);
     return ApiResponse.ok(response);
   }
 
-  @PostMapping("/{id}/apply")
+  @PostMapping("/contract-modifications/{id}/apply")
   public ApiResponse<String> applyModification(@PathVariable Long id, @RequestBody ContractModificationDetail requestDto) {
     adminService.applyModification(id, requestDto);
     return ApiResponse.ok("수정 성공");
   }
+
+  @GetMapping("/user-statics")
+  public ApiResponse<AdminStatsResponse> getAdminDashboardStats() {
+    AdminStatsResponse stats = adminService.getDashboardStats();
+    return ApiResponse.ok(stats);
+  }
 }
+
+
