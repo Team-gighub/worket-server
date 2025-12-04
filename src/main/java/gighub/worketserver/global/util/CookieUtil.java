c98bf92374d7945426e3933f797aac648a56dab1
ye0ngeun <duddms2415@naver.com>
2025-12-04 16:13:35 +0900

    refactor: 쿠키 설정 환경 변수 기반으로 전환

--- src/main/java/gighub/worketserver/global/util/CookieUtil.java
+++ src/main/java/gighub/worketserver/global/util/CookieUtil.java
@@ -1,18 +1,33 @@
 package gighub.worketserver.global.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CookieUtil {
 
+  @Value("${COOKIE_DOMAIN:}")
+  private String cookieDomain;
+
+  @Value("${COOKIE_SAME_SITE:Lax}")
+  private String sameSite;
+
+  @Value("${COOKIE_SECURE:false}")
+  private boolean secure;
+
   public ResponseCookie createTokenCookie(String name, String value, long maxAgeSeconds) {
+    // 로컬에서는 domain 비움
+    String domainToSet = (cookieDomain == null || cookieDomain.isBlank())
+      ? null
+      : cookieDomain;
+
     return ResponseCookie.from(name, value)
       .httpOnly(true)
-      .secure(true)
-      .sameSite("None")
+      .secure(secure)
+      .sameSite(sameSite)
       .path("/")
-      .domain("worket.site")
+      .domain(domainToSet)
       .maxAge(maxAgeSeconds)
       .build();
   }
--- src/main/java/gighub/worketserver/global/util/StateCookieUtil.java
+++ src/main/java/gighub/worketserver/global/util/StateCookieUtil.java
@@ -1,20 +1,28 @@
 package gighub.worketserver.global.util;
 
+import net.minidev.json.JSONUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StateCookieUtil {
 
-  private static final String DOMAIN = "worket.site";
+  @Value("${COOKIE_DOMAIN:}")
+  private String cookieDomain;
+
+  @Value("${COOKIE_SECURE:false}")
+  private boolean secure;
 
   public ResponseCookie createStateCookie(String name, String value, long maxAgeSeconds) {
+    System.out.println("cookieDomain: " + cookieDomain);
+    System.out.println("secure: " + secure);
     return ResponseCookie.from(name, value)
       .httpOnly(true)
-      .secure(true)
-      .sameSite("Lax")     // ★ 핵심: AuthorizationRequest state는 Lax여야 정상 작동
+      .secure(secure)
+      .sameSite("Lax")     // 핵심: AuthorizationRequest state는 Lax여야 정상 작동
       .path("/")
-      .domain(DOMAIN)
+      .domain(cookieDomain)
       .maxAge(maxAgeSeconds)
       .build();
   }
@@ -22,10 +30,10 @@ public class StateCookieUtil {
   public ResponseCookie deleteStateCookie(String name) {
     return ResponseCookie.from(name, "")
       .httpOnly(true)
-      .secure(true)
+      .secure(secure)
       .sameSite("Lax")
       .path("/")
-      .domain(DOMAIN)
+      .domain(cookieDomain)
       .maxAge(0)
       .build();
   }
